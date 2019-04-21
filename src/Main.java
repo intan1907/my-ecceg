87b27fe270520d7487ad93efe76716ce8e5d9bda
Rizki Alif <alifalfarisy73@gmail.com>
2019-04-22 06:40:11 +0700

    Add hash to long function

--- src/Main.java
+++ src/Main.java
@@ -62,8 +62,37 @@ public class Main {
             
             
             System.out.println("Signing...");
-            int k = new Random().nextInt();
-            long R = curve.multiplyPoint(k, curve.getBasePoint())[0];
+            Prime pri = new Prime();
+            NumberFormatter numFor = new NumberFormatter();
+            int k = new Random().nextInt((int)curve.getP()) ;;
+            long R = -1, S = -1;
+            
+            while (R <= 0 || S <=0){
+                k = new Random().nextInt((int)curve.getP()) ;
+                R = (k * curve.getBasePoint()[0]) % EllipticCurve.DEFAULT_P;
+                System.out.println(numFor.parseHashToLong(coba) + R * privateKey);
+                S = (pri.getInverse(k,EllipticCurve.DEFAULT_P) * (numFor.parseHashToLong(coba) + R * privateKey) ) % EllipticCurve.DEFAULT_P;
+            }
+            
+            
+            System.out.println("R = " + R);
+            System.out.println("S = " + S);
+            System.out.println("k = " + k);
+            
+            
+            long W = pri.getInverse(S,EllipticCurve.DEFAULT_P) % EllipticCurve.DEFAULT_P;
+            long u1 = (W*numFor.parseHashToLong(coba)) % EllipticCurve.DEFAULT_P;
+            long u2 = (W*R)% EllipticCurve.DEFAULT_P;
+            long tes =  (W * (numFor.parseHashToLong(coba) + R * privateKey) ) % EllipticCurve.DEFAULT_P;
+            System.out.println("tes = " + tes);
+            System.out.println("W = " + W);
+            System.out.println("u1 = " + u1);
+            System.out.println("u2 = " + u2);
+            System.out.println((u1 * curve.getBasePoint()[0] + u2 * publicKey[0]) % EllipticCurve.DEFAULT_P);
+            System.out.println(curve.multiplyPoint(u1, curve.getBasePoint())[0]);
+            System.out.println(curve.multiplyPoint(u2, publicKey)[0]);
+            long Verif = (curve.addPoint(curve.multiplyPoint(u1, curve.getBasePoint()),curve.multiplyPoint(u2, publicKey))[0]);
+            System.out.println(Verif);
 	}
 
 }
--- src/util/NumberFormatter.java
+++ src/util/NumberFormatter.java
@@ -96,4 +96,53 @@ public class NumberFormatter {
 		
 		return parseBinaryToLong(t);
 	}
+        
+        public static long parseHashToLong(String s) {
+		
+		String t = "";
+		for (int i = 0; i < 8; i++) {
+			switch (s.charAt(i)) {
+			case '0':
+				t += "0000"; break;
+			case '1':
+				t += "0001"; break;
+			case '2':
+				t += "0010"; break;
+			case '3':
+				t += "0011"; break;
+			case '4':
+				t += "0100"; break;
+			case '5':
+				t += "0101"; break;
+			case '6':
+				t += "0110"; break;
+			case '7':
+				t += "0111"; break;
+			case '8':
+				t += "1000"; break;
+			case '9':
+				t += "1001"; break;
+			case 'A':
+			case 'a':
+				t += "1010"; break;
+			case 'B':
+			case 'b':
+				t += "1011"; break;
+			case 'C':
+			case 'c':
+				t += "1100"; break;
+			case 'D':
+			case 'd':
+				t += "1101"; break;
+			case 'E':
+			case 'e':
+				t += "1110"; break;
+			case 'F':
+			case 'f':
+				t += "1111"; break;
+			}
+		}
+		
+		return parseBinaryToLong(t);
+	}
 }
